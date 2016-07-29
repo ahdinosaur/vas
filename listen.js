b1ac83852b122adf1c69590bac5ac9d254559654
Michael Williams <michael.williams@enspiral.com>
2016-07-29 17:38:46 +1200

    add option.getId `(ws, cb) => cb(err, id)` to vas.listen
    use case is to assign user id from ws.headers.cookie like so:
    
    ```js
    tickets.check(ws.headers.cookie, function (err, id) {
      if (err) console.error(err)
        cb(null, id)
      })
    })
    ```
    
    where `tickets` is an instance of https://github.com/dominictarr/ticket-auth

--- listen.js
+++ listen.js
@@ -13,17 +13,31 @@ function listen (api, config, options) {
 
   const port = defined(options.port, DEFAULT_PORT)
   const onListen = options.onListen
+  const getId = defined(options.getId, defaultGetId)
 
   const server = createServer(api, config)
   const ws = Ws.createServer(options, onConnect)
 
   return ws.listen(port, onListen)
 
-  function onConnect (stream) {
-    pull(
-      stream,
-      server.createStream(),
-      stream
-    )
+  function onConnect (ws) {
+    getId(ws, function (err, id) {
+      if (err) {
+        return pull(
+          pull.error(err),
+          ws.sink
+        )
+      }
+
+      pull(
+        ws,
+        server.createStream(id),
+        ws
+      )
+    })
   }
+}
+
+function defaultGetId (ws, cb) {
+  cb(null, null)
 }
